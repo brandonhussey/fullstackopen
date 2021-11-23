e683ad8b5fda7425d447ee05a6643a46c4bfc842
brandonhussey <brandon_hussey@icloud.com>
2021-11-23 13:33:06 -0400

    solution for 2.8

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 import Person from "./Components/Person";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "123 456 7890" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  const addName = (e) => {
+  const addPerson = (e) => {
     e.preventDefault();
     const personObject = {
       name: newName,
+      number: newNumber,
     };
 
     if (
@@ -21,18 +25,25 @@ const App = () => {
       setPersons(persons.concat(personObject));
     }
     setNewName("");
+    setNewNumber("");
   };
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
   };
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          phone: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
--- Part 2/phonebook/src/Components/Person.js
+++ Part 2/phonebook/src/Components/Person.js
@@ -3,7 +3,9 @@ import React from "react";
 const Person = ({ person }) => {
   return (
     <div>
-      <h3>{person.name}</h3>
+      <h3>
+        {person.name} {person.number}
+      </h3>
     </div>
   );
 };
